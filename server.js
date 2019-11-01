90db7af39b7fadf4dc68d93ad62717f28f1bd402
CarnunMP <carnun@hotmail.co.uk>
2019-11-01 08:55:59 +0000

    logger set up

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const middleware = require('./middleware');
 
 const server = express();
 
 server.use(express.json());
+server.use(middleware.logger);
 
 server.get('/', (req, res) => {
   res.send("<h2>Let's gooooo...!<h2>");
