9836b56462e1bbc98e29a7e8c031dcc38c98029d
CarnunMP <carnun@hotmail.co.uk>
2019-11-01 09:35:21 +0000

    GET projects/:id/actions done

--- middleware.js
+++ middleware.js
@@ -26,7 +26,27 @@ exports.validateProjectId = function(req, res, next) {
       res.status(500).json({
         message: 'validateProjectId failed: ' + err.message,
       });
-    })
+    });
 }
 
 // action middleware
+exports.validateActionId = function(req, res, next) {
+  const { id } = req.params;
+
+  actionDb.get(id)
+    .then(action => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        res.status(400).json({
+          message: 'invalid action id',
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'validateActionId failed: ' + err.message,
+      });
+    });
+}
--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const actionDb = require('../data/helpers/actionModel');
+const projectDb = require('../data/helpers/projectModel');
 const middleware = require('../middleware');
 
 const router = express.Router();
-// router.use('/:id', middleware.validateActionId);
+router.use('/:id', middleware.validateActionId);
 
-// CRUD here
+router.get('/', (req, res) => {
+  const { id } = req.project;
+
+  projectDb.getProjectActions(id)
+  .then(actions => {
+    // TODO: What if the project exists, but there are no actions??
+    res.status(200).json(actions);
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: `GET /projects/${id}/actions: ` + err.message,
+    });
+  });
+});
 
 module.exports = router;
